ce8ba40564c0ba65e3271d22defe18f52a56cfb9
bgallagher-9 <b.gallagher9@gmail.com>
2017-04-04 13:32:55 -0500

    day 17 warmup, css ani.trani, & multi toggle click edits

--- day-16-multi-toggle-on-click/script.js
+++ day-16-multi-toggle-on-click/script.js
@@ -1,38 +1,54 @@
-var bricir = document.querySelector('#brian');
-var megcir = document.querySelector('#megan');
-var juncir = document.querySelector('#june');
-var milcir = document.querySelector('#milo');
-var dulcir = document.querySelector('#dulce');
+// var bricir = document.querySelector('#brian');
+// var megcir = document.querySelector('#megan');
+// var juncir = document.querySelector('#june');
+// var milcir = document.querySelector('#milo');
+// var dulcir = document.querySelector('#dulce');
+//
+// function theToggleBri() {
+//   bricir.classList.toggle('togciron');
+//   //DONT NEST AN EVENTLISTENER INSIDE AN EVENTLISTENER
+//   //it will create new eventlisteners that you dont want
+//   //---------------------------
+//   // function theToggle2() {
+//   //   bricir.classList.toggle('togciroff');
+//   //   console.log(bricir);
+//   // }
+//   // bricir.addEventListener('click', theToggle2);
+// }
+// bricir.addEventListener('click', theToggleBri);
+//
+// function theToggleMeg() {
+//   megcir.classList.toggle('togciron');
+// }
+// megcir.addEventListener('click', theToggleMeg);
+//
+// function theToggleJun() {
+//   juncir.classList.toggle('togciron');
+// }
+// juncir.addEventListener('click', theToggleJun);
+//
+// function theToggleMil() {
+//   milcir.classList.toggle('togciron');
+// }
+// milcir.addEventListener('click', theToggleMil);
+//
+// function theToggleDul() {
+//   dulcir.classList.toggle('togciron');
+// }
+// dulcir.addEventListener('click', theToggleDul);
+//
 
-function theToggleBri() {
-  bricir.classList.toggle('togciron');
-  //DONT NEST AN EVENTLISTENER INSIDE AN EVENTLISTENER
-  //it will create new eventlisteners that you dont want
-  //---------------------------
-  // function theToggle2() {
-  //   bricir.classList.toggle('togciroff');
-  //   console.log(bricir);
-  // }
-  // bricir.addEventListener('click', theToggle2);
-}
-bricir.addEventListener('click', theToggleBri);
+var getButton = document.querySelector('#add-thing');
+var getOl = document.querySelector('ol');
+getOl.addEventListener('click', function(eventname) {
+  if (eventname.target.tagName == 'LI') {
+    eventname.target.classList.toggle('togciron');
+  }
+});
 
-function theToggleMeg() {
-  megcir.classList.toggle('togciron');
-}
-megcir.addEventListener('click', theToggleMeg);
+getButton.addEventListener('click', function() {
+  var ola = document.createElement('li');
+  ola.textContent = 'new';
+  getOl.appendChild(ola);
 
-function theToggleJun() {
-  juncir.classList.toggle('togciron');
-}
-juncir.addEventListener('click', theToggleJun);
-
-function theToggleMil() {
-  milcir.classList.toggle('togciron');
-}
-milcir.addEventListener('click', theToggleMil);
-
-function theToggleDul() {
-  dulcir.classList.toggle('togciron');
-}
-dulcir.addEventListener('click', theToggleDul);
+});
---
+++ day-17-warmup/script.js
@@ -0,0 +1,66 @@
+
+var groceries = [
+  { name: 'Dr Pepper', cost: 0.99 },
+  { name: 'ribeye', cost: 12.78 },
+  { name: 'gum', cost: 1.12 },
+  { name: 'flowers', cost: 7.99 },
+  { name: 'pork chops', cost: 8.50 },
+  { name: 'eggs', cost: 3.40 },
+  { name: 'milk', cost: 5.20 },
+  { name: 'lip balm', cost: 0.99 },
+  { name: 'tissues', cost: 1.30 },
+  { name: 'toothbrush', cost: 3.00 },
+  { name: 'Claritin', cost: 12.36 },
+  { name: 'Tic-Tacs', cost: 1.85 }
+];
+
+
+//Make this function calculate and return the average cost.
+var totalCost = 0;
+function averageCost(items) {
+  var space = '';
+  for (var i = 0; i < items.length; i++) {
+    totalCost += items[i].cost;
+    // console.log(totalCost);
+  }
+}
+var space = averageCost(groceries);
+// console.log('average', average);
+
+var space = document.querySelector('#costavg');
+space.textContent = totalCost/groceries.length;
+
+//Make this function return the most expensive item.
+// var mostExpensiveItem = [0];
+function mostExpensive(items) {
+  var mostExpensiveItem = items[0];
+  for (var i = 1; i < items.length; i++) {
+    if (items[i].cost > mostExpensiveItem.cost) {
+      mostExpensiveItem = items[i];
+    }
+  }
+  return mostExpensiveItem;
+}
+var mostExpensiveItem = mostExpensive(groceries);
+console.log('most expensive', mostExpensiveItem);
+
+var mostExp = document.querySelector('#expensiveItem');
+mostExp.textContent = mostExpensiveItem.name;
+// console.log(groceries.name);
+
+//Make this function return the least expensive item.
+// var leastExpensiveItem = [0];
+function leastExpensive(items) {
+  var leastExpensiveItem = items[0];
+  for (var i = 0; i < items.length; i++) {
+    if (items[i].cost < leastExpensiveItem.cost) {
+      leastExpensiveItem = items[i];
+    }
+  }
+  return leastExpensiveItem;
+}
+var leastExpensiveItem = leastExpensive(groceries);
+console.log('least expensive', leastExpensiveItem);
+
+var leastExp = document.querySelector('#expensiveItemLeast');
+leastExp.textContent = leastExpensiveItem.name;
