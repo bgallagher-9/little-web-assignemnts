96098d076610a7b1f7f0257df3cb333b95d8258f
bgallagher-9 <b.gallagher9@gmail.com>
2017-04-10 16:43:39 -0500

    sprite animation

--- day-20-sprite-animation/script.js
+++ day-20-sprite-animation/script.js
@@ -1,66 +1,66 @@
 var fire1 = document.querySelector('.fire1');
 var fire2 = document.querySelector('.fire2');
+var boom = document.querySelector('#boom');
 var burnit = document.querySelector('#burnit');
 var positionX = 0;
 var positionY = 0;
-var frameWidth = 100;
-// var positionY = 0;
-var intervalId;
+var size = 128;
+var size2 = 320;
+var row = 0;
+var column = 0;
+var intervalId = 0;
+var row2 = 0;
+var column2 = 0;
+var intervalId2 = 0;
 
-// var direction = 'left';
-
-// window.addEventListener('keydown', function(eventname) {
-//   console.log(eventname.keyCode);
-//   if (eventname.keyCode === 37) {
-//   // direction = 'left';
-//   positionY = -25.5;
-//   }
-// });
-
-// burnit.addEventListener('click', function(){
-//   console.log('stop');
-//   clearInterval(intervalId);
-// });
-
-burnit.addEventListener('click', function(){
+boom.addEventListener('click', function(){
   if (intervalId === undefined) {
     intervalId = setInterval(function() {
-      fire1.style.backgroundPositionX = positionX + 'px';
-      positionX -= 100;
-      if (positionX >= frameWidth) {
-        positionX = 0;
-      }
-    }, 80);
-    burnit.textContent = 'STOP';
+    positionX = column * size * -1;
+    positionY = row * size * -1;
+    fire1.style.backgroundPositionX = positionX + 'px';
+    fire1.style.backgroundPositionY = positionY + 'px';
+    column += 1;
+    if (column === 8) {
+      column = 0;
+      row += 1;
+    };
+    if (row === 8) {
+      column += 1;
+      row = 0;
+    }
+  }, 20);
+    boom.textContent = 'STOP';
   }
   else {
     clearInterval(intervalId);
     intervalId = undefined;
-    burnit.textContent = 'Burn It!';
+    boom.textContent = 'BOOM!';
   }
 });
 
-// function animate() {
-//   // console.log(positionX);
-//   fire1.style.backgroundPositionX = positionX + 'px';
-//   // fire1.style.backgroundPositionY = positionY + 'px';
-//   positionX -= 340;
-//   if (positionX >= frameWidth) {
-//     positionX = 0;
-//   }
-//   intervalId = setTimeout(animate, 70);
-//
-// };
-// animate();
-
-
-// function animateSprite() {
-//   spriteElement.style.backgroundPosition = curPx + 'px 0px';
-//   curPx = curPx + frameWidth;
-//   if (curPx >= spriteWidth) {
-//   curPx = 0;
-//   }
-//   ti = setTimeout(animateSprite, 80);
-//   }
-//   animateSprite();
-// });
+burnit.addEventListener('click', function(){
+  if (intervalId2 === undefined) {
+    intervalId2 = setInterval(function() {
+    positionX = column2 * size2 * -1;
+    positionY = row2 * size2 * -1;
+    fire2.style.backgroundPositionX = positionX + 'px';
+    fire2.style.backgroundPositionY = positionY + 'px';
+    column2 += 1;
+    if (column2 === 4) {
+      column2 = 0;
+      row2 += 1;
+    };
+    if (row2 === 4) {
+      column2 += 1;
+      row2 = 0;
+    }
+  }, 50);
+    burnit.textContent = 'STOP';
+  }
+  else {
+    clearInterval(intervalId2);
+    intervalId2 = undefined;
+    burnit.textContent = 'Burn It!';
+  }
+});
