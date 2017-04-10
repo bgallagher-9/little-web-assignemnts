02a3ce3c5d8185548f865a123fd0fd08ac210886
bgallagher-9 <b.gallagher9@gmail.com>
2017-04-10 18:54:51 -0500

    more ajax, swapi pages

--- day-21-first-ajax/script.js
+++ day-21-first-ajax/script.js
@@ -1,19 +1,25 @@
 // console.log($);
 var jqprom = $.ajax({
   url: 'https://swapi.co/api/people/'
+  // url: 'https://swapi.co/api/people/?page2/'
 });
 
 var peopleList = document.querySelector('#people-list');
+var body = document.querySelector('body');
+var title = document.createElement('h1');
+title.setAttribute("class", "title");
+title.textContent = 'Characters of Star Wars';
+
 
 jqprom.done(function(data) {
-  console.log('got it', data)
+
 
   for (var i = 0; i < data.results.length; i++) {
-    console.log('each object', data.results[i])
     var swapili = document.createElement('li');
     var swapih2 = document.createElement('h2');
     var swapidivgen = document.createElement('div');
     var swapidivhair = document.createElement('div');
+
     swapih2.textContent = data.results[i].name;
     swapidivgen.textContent = 'Gender = ' +  data.results[i].gender;
     swapidivhair.textContent = 'Hair Color = ' + data.results[i].hair_color;
@@ -21,5 +27,16 @@ jqprom.done(function(data) {
     swapili.appendChild(swapih2);
     swapili.appendChild(swapidivgen);
     swapili.appendChild(swapidivhair);
+
   }
+  body.appendChild(title);
+  var nextDiv = document.createElement('div');
+  nextDiv.setAttribute("class", "nextDiv");
+  var nextPeople = document.createElement('a');
+
+  nextPeople.textContent = 'Next Page';
+  nextPeople.href = data.next;
+  body.appendChild(nextDiv);
+  nextDiv.appendChild(nextPeople);
+  // console.log(data.next);
 });
