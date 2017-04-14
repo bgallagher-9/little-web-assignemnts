18e28047f0490f20806dcb86ed86c9df89e7c823
bgallagher-9 <b.gallagher9@gmail.com>
2017-04-14 16:12:16 -0500

    day 24 more scorecard filtering

--- day-24-scorecard/public/script.js
+++ day-24-scorecard/public/script.js
@@ -1,24 +1,24 @@
 var tbody = document.querySelector('tbody');
 var ajaxdata;
-
 function putOnPage(list) {
   for (var i = 0; i < ajaxdata.values.length; i++) {
-    var tr = document.createElement('tr');
-    var tdinst = document.createElement('td');
-    var tdact = document.createElement('td');
-    tdinst.textContent = ajaxdata.values[i].Name;
-    tdact.textContent = ajaxdata.values[i].Act75;
-    tr.appendChild(tdinst);
-    tr.appendChild(tdact);
-    tbody.appendChild(tr);
-    // console.log(ajaxdata.values[i]);
+    if (ajaxdata.values[i].Act75 === 'NULL') {
+      continue;
+    }
+      var tr = document.createElement('tr');
+      var tdinst = document.createElement('td');
+      var tdact = document.createElement('td');
+      tdinst.textContent = ajaxdata.values[i].Name;
+      tdact.textContent = ajaxdata.values[i].Act75;
+      tr.appendChild(tdinst);
+      tr.appendChild(tdact);
+      tbody.appendChild(tr);
   }
 }
 $.ajax({
   url: '/api/scorecard'
 })
 .done(function(data) {
-  // console.log(data);
   ajaxdata = data;
-  putOnPage(ajaxdata.values);
+putOnPage(ajaxdata.values);
 });
