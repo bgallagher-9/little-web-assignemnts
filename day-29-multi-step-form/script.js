dae2340fe098692bdeecbb5b97380f9316241a54
bgallagher-9 <b.gallagher9@gmail.com>
2017-04-26 17:42:47 -0500

    day 29 multi step form

--- day-29-multi-step-form/script.js
+++ day-29-multi-step-form/script.js
@@ -1,7 +1,7 @@
 var namesDiv = document.querySelector('#names');
 var genderDiv = document.querySelector('.gender');
 var selectDiv = document.querySelector('#select-menu');
-var textDiv = document.querySelector('#textarea');
+var textDiv = document.querySelector('.textclassarea');
 var nextName = document.querySelector('#next-button');
 var nextGender = document.querySelector('#next-gender-button');
 var nextSelect = document.querySelector('#select-menu-button');
@@ -12,9 +12,11 @@ var prevText = document.querySelector('#previous-text-button');
 var firstName = document.querySelector('#first-name');
 var lastName = document.querySelector('#last-name');
 var missingName = document.querySelector('#missing-name');
-var genderChoice = document.querySelector('.choice-of-gender');
 var select = document.querySelector('select');
-var option = document.querySelector('option');
+// var option = document.querySelector('option');
+var gender = document.querySelector('#gender').getElementsByTagName('INPUT');
+var genderChecked = localStorage.getItem('gender');
+var textFill = document.querySelector('#textidarea').getElementsByTagName('TEXTAREA');
 
 
 
@@ -33,131 +35,61 @@ nextName.addEventListener('click', function(evt) {
   localStorage.setItem('nameLast', lastName.value)
 });
 
-
-var male = document.querySelector('#male');
-var female = document.querySelector('#female');
-var bothgen = document.querySelector('#bothgen');
-var dunnogen = document.querySelector('#dunnogen');
-var nagen = document.querySelector('#nagen');
-var gender = document.querySelector('#gender').getElementsByTagName('INPUT');
-
-
-
- console.log(gender[3].id);
-
-
-// male.checked = localStorage.getItem('maleRadio');
-// female.checked = localStorage.getItem('femaleRadio');
-// bothgen.checked = localStorage.getItem('bothRadio');
-// dunnogen.checked = localStorage.getItem('dunnoRadio');
-// nagen.checked = localStorage.getItem('naRadio');
-// console.log(gender);
+for (var i = 0; i < gender.length; i++) {
+  if (gender[i].id === genderChecked) {
+    gender[i].checked = true;
+    break;
+  }
+}
 nextGender.addEventListener('click', function(evt) {
   evt.preventDefault();
-  for (var i = 0; i < gender.length; i++) {
-    if (gender[i].id === gender.checked) {
-      localStorage.setItem('gender', gender.id.checked)
-    }
-    // console.log(gender[i].id, gender[i].checked);
-    console.log(gender[i].id.checked);
-  }
-  // for (var i = 0; i < genderDiv.length; i++) {
-  //   if (genderDiv[i].input === genderDiv.input.checked) {
-  //   localStorage.setItem('gender', genderDiv.id.checked)
-  //   }
-  //   console.log(genderDiv.input[i].checked);
-  // }
-  // console.log(genderDiv.input.checked);
-  //queryselectorall to loop thru and find the one that is checked instead of the if/else if...
-  // console.log(evt);
   genderDiv.style.display = 'none';
   selectDiv.style.display = 'block';
-  // if (male.checked === true) {
-  //   // console.log('male', male.checked);
-  //   localStorage.setItem('gender', male.checked)
-  // }
-  // else if (female.checked === true) {
-  //   // console.log('female', female.checked);
-  //   localStorage.setItem('gender', female.checked)
-  // }
-  // else if (bothgen.checked === true) {
-  //   // console.log('both', bothgen.checked);
-  //   localStorage.setItem('gender', bothgen.checked)
-  // }
-  // else if (dunnogen.checked === true) {
-  //   // console.log('dunno', dunnogen.checked);
-  //   localStorage.setItem('gender', dunnogen.checked)
-  // }
-  // else if (nagen.checked === true) {
-  //   // console.log('NA', nagen.checked);
-  //   localStorage.setItem('gender', nagen.checked)
-  // }
+  for (var i = 0; i < gender.length; i++) {
+    if (gender[i].checked) {
+      localStorage.setItem('gender', gender[i].id)
+      break;
+    }
+  //queryselectorall to loop thru and find the one that is checked instead of the if/else if...
+  };
 });
-
 prevGender.addEventListener('click', function(evt) {
   evt.preventDefault();
   namesDiv.style.display = 'block';
   genderDiv.style.display = 'none';
-  if (male.checked === true) {
-    // console.log('male', male.checked);
-    localStorage.setItem('gender', male.checked)
-  }
-  else if (female.checked === true) {
-    // console.log('female', female.checked);
-    localStorage.setItem('gender', female.checked)
-  }
-  else if (bothgen.checked === true) {
-    // console.log('both', bothgen.checked);
-    localStorage.setItem('gender', bothgen.checked)
-  }
-  else if (dunnogen.checked === true) {
-    // console.log('dunno', dunnogen.checked);
-    localStorage.setItem('gender', dunnogen.checked)
-  }
-  else if (nagen.checked === true) {
-    // console.log('NA', nagen.checked);
-    localStorage.setItem('gender', nagen.checked)
+  for (var i = 0; i < gender.length; i++) {
+    if (gender[i].checked) {
+      localStorage.setItem('gender', gender[i].id)
+      break;
+    }
   }
 });
-// var value;
-// value = localStorage.getItem('color');
-
 
 select.value = localStorage.getItem('color');
 
 if (localStorage.getItem('color') === 'red') {
   red.selected;
-  // console.log('red', red.selected);
 }
 else if (localStorage.getItem('color') === 'black') {
   black.selected;
-  // console.log('black', black.selected);
 }
 else if (localStorage.getItem('color') === 'yellow') {
   yellow.selected;
-  // console.log('yellow', yellow.selected);
 }
 else if (localStorage.getItem('color') === 'green') {
-  // select.value === 'green';
-  console.log('green', green.selected);
-  // console.log(select.value);
+  select.value === 'green';
 }
 else if (localStorage.getItem('color') === 'no') {
   no.selected;
-  console.log('no', no.selected);
 };
 
-// console.log(localStorage.getItem('color'));
 prevSelect.addEventListener('click', function(evt) {
   evt.preventDefault();
   genderDiv.style.display = 'block';
   selectDiv.style.display = 'none';
   var colors = document.getElementsByName('colors')[0]
   var value = colors.options[colors.selectedIndex].value;
-
-  // console.log(value);
   localStorage.setItem('color', value);
-  // console.log(select.value)
 });
 nextSelect.addEventListener('click', function(evt) {
   evt.preventDefault();
@@ -165,9 +97,18 @@ nextSelect.addEventListener('click', function(evt) {
   selectDiv.style.display = 'none';
   var colors = document.getElementsByName('colors')[0]
   var value = colors.options[colors.selectedIndex].value;
-  // console.log(value);
   localStorage.setItem('color', value);
-  // console.log(select)
+});
+
+
+//needs more work*********
+
+// var textArea = document.querySelector('textarea');
+console.log(textFill);
+textFill = localStorage.getItem('text');
+textDiv.addEventListener('keyup', function(evt) {
+  // localStorage.setItem('text', textFill.textContent);
+  console.log(textDiv.textarea.value);
 });
 prevText.addEventListener('click', function(evt) {
   evt.preventDefault();
