ffb91b67ca35fe4cafc6babcc06f7a930196dc2d
bgallagher-9 <b.gallagher9@gmail.com>
2017-05-08 15:49:43 -0500

    todo app clean up - works, needs styling

--- day-39-react-todo/src/todoapp.js
+++ day-39-react-todo/src/todoapp.js
@@ -26,32 +26,32 @@ class TodoApp extends Component {
     })
   }
 
-  getNewItem(todoText) {
+  getNewItem() {
     $.ajax({
       url: `https://spiffy-todo-api.herokuapp.com/api/items?bucketId=${bucketId}`,
-      method: 'GET',
-      data: {
-        text: todoText
-      }
-    })
+      // method: 'GET',
+      // data: {
+      //   text: todoText
+      })
+
     .done((data) => {
       console.log(data);
       this.setState({
         items: data.items
-      })
-    })
+      });
+    });
   }
 
   componentDidMount() {
     this.getNewItem();
   }
 
-  removeNewItem(id, evt) {
+  removeNewItem(id) {
     $.ajax({
       url: `https://spiffy-todo-api.herokuapp.com/api/item/${id}?bucketId=${bucketId}`,
       method: 'DELETE',
     })
-    .done(() => {
+    .done((data) => {
       this.getNewItem();
     })
   }
@@ -62,15 +62,22 @@ class TodoApp extends Component {
   //   });
   // }
 
-  handleKeyUp(evt) {
-
-    if (evt.keyCode === 13) {
+  handleKeyUp(keyCode) {
+    if (keyCode === 13) {
       this.createNewItem(this.state.todovalue);
-      this.getNewItem(this.state.todovalue);
+      // this.getNewItem(this.state.todovalue);
       this.setState({
         todovalue: ''
-      });
 
+      });
+      // handleKeyUp(keyCode) {
+      //     if (keyCode === 13) {
+      //       this.createItem(this.state.inputValue);
+      //       this.setState({
+      //         inputValue: ''
+      //       });
+      //     }
+      //   }
     }
   }
   toggleItem(id) {
@@ -78,26 +85,24 @@ class TodoApp extends Component {
       url: `https://spiffy-todo-api.herokuapp.com/api/item/${id}/togglestatus?bucketId=${bucketId}`,
       method: 'POST',
     })
-    .done(() => {
+    .done((data) => {
       this.getNewItem();
     })
   }
 
   render() {
-
-
+console.log(this.state)
     let todoItems = this.state.items.map((x) => {
-
       const className = x.isComplete ? 'complete' : '';
-
       return <li className={className} key={x.id} onClick={() => this.toggleItem(x.id)}>{x.text}<button onClick={(evt) => this.removeNewItem(x.id, evt)}>Delete</button></li> })
 
     return(
       <div>
         <input
-          onKeyUp={(evt) => this.handleKeyUp(evt.keyCode)}
-          onChange={(evt) => this.handleChange(evt)}
           value={this.state.todovalue}
+          onKeyUp={(evt) => this.handleKeyUp(evt.keyCode)}
+          onChange={(evt) => this.setState({ todovalue: evt.target.value})}
+
         />
       <ol>
         {todoItems}
